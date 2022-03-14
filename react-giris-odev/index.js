6a5aaa25398b1d1de4f150ce7ae2257e21ce5e43
Berkay Cem İzer <94552755+BerkayCemIzer@users.noreply.github.com>
2022-03-14 15:54:36 +0300

    Delete react-giris-odev directory

--- react-giris-odev/index.js
+++
@@ -1,3 +0,0 @@
-import getData from './app.js'; // app.js'ye yazdığımız fonksiyonu default çağırdık.
-
-getData(1); // Fonksiyonumuzu çalıştırırken 1 bilgisini fonksiyonumuzdaki user_id parametresine yolladık.
