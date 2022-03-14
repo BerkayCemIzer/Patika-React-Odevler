1e425ce93e88cb3b47d6b1c40891a1e2a4357808
Berkay Cem İzer <94552755+BerkayCemIzer@users.noreply.github.com>
2022-03-14 15:55:51 +0300

    index, package, package-lock, app added

---
+++ Giris-Odev/index.js
@@ -0,0 +1,3 @@
+import getData from './app.js'; // app.js'ye yazdığımız fonksiyonu default çağırdık.
+
+getData(1); // Fonksiyonumuzu çalıştırırken 1 bilgisini fonksiyonumuzdaki user_id parametresine yolladık.
